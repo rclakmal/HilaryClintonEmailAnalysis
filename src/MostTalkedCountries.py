c6ff83cef5990aa1e9bacce9ad345a5556ef642c
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-01-16 00:21:31 +0100

    adding results of most common words

--- src/MostTalkedCountries.py
+++ src/MostTalkedCountries.py
@@ -1,29 +1,21 @@
-'''
-Created on Jan 15, 2016
-
-@author: rclakmal
-'''
-
 import pandas as pd
-import enchant
 import re
 import csv
 from collections import Counter
 
+#Writing
 word_list = []
 email_dataframe = pd.read_csv("../input/Emails.csv")
 raw_text_list = email_dataframe['RawText'].tolist()
 for raw_text in raw_text_list:
     word_list = word_list + re.findall('\w+', raw_text.lower())
-english_checker = enchant.Dict("en_US")
+
+#Using pyCountry module to list down all the countries. Used only the first 3 letters of the country since most
 high_frequency_words = Counter(x for x in word_list if not english_checker.check(x)).most_common(100)
 
-with open("../output/100mostcommonwords.csv", 'w') as csvfile:
+with open("../output/mostcommoncountries.csv", 'w') as csvfile:
     fieldnames = ['Word', 'Count']
     writer = csv.writer(csvfile)
     writer.writerow(fieldnames)
-    for word_count_pair in high_frequency_words:
-        word = word_count_pair[0]
-        count = word_count_pair[1]
-        row = [word,count]
-        writer.writerow(row)
+    for key, value in high_frequency_words.iteritems():
+        writer.writerow(key + value)
