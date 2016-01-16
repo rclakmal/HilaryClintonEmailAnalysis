a9ade026331282c57488fbfb566794a1abb92e9e
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-01-16 02:05:37 +0100

    Add most common country script

--- src/MostCommonWords.py
+++ src/MostCommonWords.py
@@ -10,14 +10,18 @@ import re
 import csv
 from collections import Counter
 
+# Reading all the text in raw email body and partitioning to words.
 word_list = []
 email_dataframe = pd.read_csv("../input/Emails.csv")
 raw_text_list = email_dataframe['RawText'].tolist()
 for raw_text in raw_text_list:
     word_list = word_list + re.findall('\w+', raw_text.lower())
+
+# Considering non-language specific words checking for top 100 high frequncy words.
 english_checker = enchant.Dict("en_US")
 high_frequency_words = Counter(x for x in word_list if not english_checker.check(x)).most_common(100)
 
+# Saving to a CSV.
 with open("../output/100mostcommonwords.csv", 'w') as csvfile:
     fieldnames = ['Word', 'Count']
     writer = csv.writer(csvfile)
@@ -25,5 +29,5 @@ with open("../output/100mostcommonwords.csv", 'w') as csvfile:
     for word_count_pair in high_frequency_words:
         word = word_count_pair[0]
         count = word_count_pair[1]
-        row = [word,count]
+        row = [word, count]
         writer.writerow(row)
--- src/MostTalkedCountries.py
+++ src/MostTalkedCountries.py
@@ -1,21 +1,40 @@
 import pandas as pd
 import re
 import csv
+import enchant
+import pycountry
 from collections import Counter
 
-#Writing
+# Reading all the text in raw email body and partitioning to words.
 word_list = []
 email_dataframe = pd.read_csv("../input/Emails.csv")
 raw_text_list = email_dataframe['RawText'].tolist()
 for raw_text in raw_text_list:
     word_list = word_list + re.findall('\w+', raw_text.lower())
 
-#Using pyCountry module to list down all the countries. Used only the first 3 letters of the country since most
-high_frequency_words = Counter(x for x in word_list if not english_checker.check(x)).most_common(100)
+# Using pyCountry module to list down all the countries. We consider shorten forms of the countries as well.
+# Example: We consider all 3 formats of pakistan. (pakistan, pk, pak).
+# But if it is a language specific word such as 'us', we ignore those items from our search.
+country_list = []
+english_checker = enchant.Dict("en_US")
+country_list = []
+for country in pycountry.countries:
+    country_list.append(country.name.strip().lower())
+    if not english_checker.check(country.alpha2.lower()):
+        country_list.append(country.alpha2.lower())
+    if not english_checker.check(country.alpha3.lower()):
+        country_list.append(country.alpha3.lower())
+
+high_frequency_countries = Counter(x for x in word_list if x in country_list).most_common(100)
 
 with open("../output/mostcommoncountries.csv", 'w') as csvfile:
-    fieldnames = ['Word', 'Count']
+    fieldnames = ['Country', 'Count']
     writer = csv.writer(csvfile)
     writer.writerow(fieldnames)
-    for key, value in high_frequency_words.iteritems():
-        writer.writerow(key + value)
+    for country_count_pair in high_frequency_countries:
+        country = country_count_pair[0]
+        count = country_count_pair[1]
+        row = [country, count]
+        writer.writerow(row)
+
+
